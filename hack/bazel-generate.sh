65f44f645289c122007e9d51f2ad7966ee4d6b81
Dominik Holler <dholler@redhat.com>
2025-01-27 12:52:36 +0100

    install bazel

--- hack/bazel-generate.sh
+++ hack/bazel-generate.sh
@@ -4,6 +4,12 @@ source hack/common.sh
 source hack/bootstrap.sh
 source hack/config.sh
 
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel
+
 # remove libvirt BUILD file to regenerate it each time
 rm -f vendor/libvirt.org/go/libvirt/BUILD.bazel
 
